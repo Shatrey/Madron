be7b6756e497f371d166a04abaa97f36ea2daca4
Mark <mark.denyssenko@gmail.com>
2019-12-17 00:36:35 +0200

    added walls logic and player animation

--- Madron/Assets/Scripts/Wall.cs
+++ Madron/Assets/Scripts/Wall.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class Wall : MonoBehaviour
 {
-    public int hp = 3;
+    public Sprite dmgSprite;
+	public int hp = 4;
+	public AudioClip chopSound1;
+	public AudioClip chopSound2;
+
+	private SpriteRenderer spriteRenderer;
+
+    void Awake () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
 
     public void DamageWall(int loss)
     {
-        hp -= loss;
-        if (hp <= 0)
-        {
-            gameObject.SetActive(false);
-        }
+        spriteRenderer.sprite = dmgSprite;
+		hp -= loss;
+		if (hp <= 0)
+			gameObject.SetActive (false);
     }
 }
