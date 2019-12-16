9bea297a1af10fe71f9e68857fa03187a67267fd
Shatrey <shatrikua@gmail.com>
2019-12-16 03:12:33 +0200

    adds wall script

---
+++ Madron/Assets/Scripts/Wall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour
+{
+    public int hp = 3;
+
+    public void DamageWall(int loss)
+    {
+        hp -= loss;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
