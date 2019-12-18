4168e9d4647069fefeba0bc3989293fdd26844ae
Mark <mark.denyssenko@gmail.com>
2019-12-18 23:48:41 +0200

    wall improvement

--- Madron/Assets/Scripts/Wall.cs
+++ Madron/Assets/Scripts/Wall.cs
@@ -16,6 +16,7 @@ public class Wall : MonoBehaviour
 
     public void DamageWall(int loss)
     {
+        SoundManager.instance.RandomizeSfx (chopSound1, chopSound2);
         spriteRenderer.sprite = dmgSprite;
 		hp -= loss;
 		if (hp <= 0)
